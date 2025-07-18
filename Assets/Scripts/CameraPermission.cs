8852d1d24a06c51df6dcac5d458e1d2b3d4e222c
Kaja Kutbudeen <ashrafkutbudeen@gmail.com>
2025-07-18 15:11:21 +0530

    update

---
+++ Assets/Scripts/CameraPermission.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Android;
+
+public class CameraPermission : MonoBehaviour
+{
+    void Start()
+    {
+        if (!Permission.HasUserAuthorizedPermission(Permission.Camera))
+        {
+            Permission.RequestUserPermission(Permission.Camera);
+        }
+        // You might want to delay AR initialization until permission is granted
+        // or show a UI message if permission is denied.
+    }
+}
