52764f671970dbc7e9dca4fa087601e8f654c7ec
Kaja Kutbudeen <ashrafkutbudeen@gmail.com>
2025-07-19 14:56:43 +0530

    update

--- Assets/Scripts/Debug Scripts/AddresablesBundleLoader.cs
+++ Assets/Scripts/Debug Scripts/AddresablesBundleLoader.cs
@@ -4,22 +4,31 @@ using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.InputSystem;
+using System.IO;
 public class AddresablesBundleLoader : MonoBehaviour
 {
     public string prefabKey = "Cube";
+    private InputActionSettings _InputAction;
+    private InputAction _TouchPress;
+    public List<string> FurnitureList;
 
-    
-
-
-    void Start()
+    private void OnEnable()
     {
-        Addressables.InstantiateAsync(prefabKey).Completed += OnPrefabLoaded;
+        _InputAction.Enable();
+       _TouchPress = _InputAction.Touch.TouchPressed;
+
+        _TouchPress.Enable();
+        _TouchPress.performed += TouchPress;
     }
 
-
-
-
-
+    private void TouchPress(InputAction.CallbackContext context)
+    {
+        Debug.Log("Touched");
+    }
+    void Start()
+    {      
+      //  Addressables.InstantiateAsync(prefabKey).Completed += OnPrefabLoaded;
+    }
     void OnPrefabLoaded(AsyncOperationHandle<GameObject> handle)
     {
         if (handle.Status == AsyncOperationStatus.Succeeded)
@@ -30,5 +39,12 @@ public class AddresablesBundleLoader : MonoBehaviour
         {
             Debug.LogError($" Failed to load prefab '{prefabKey}'!");
         }
+    }
+
+    private void OnDisable()
+    {
+        _InputAction.Disable();
+        _TouchPress.Disable();
+        _TouchPress.performed -= TouchPress;
     }
 }
